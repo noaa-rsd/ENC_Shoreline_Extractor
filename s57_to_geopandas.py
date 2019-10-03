e55b2832125eaab66d2baf02a34a1fd1c6382105
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-10-03 13:30:06 -0400

    started OOP refactor

--- s57_to_geopandas.py
+++ s57_to_geopandas.py
@@ -1,66 +1,101 @@
 import os
 import fiona
-from fiona._drivers import GDALEnv
 from pathlib import Path
 import geopandas as gpd
 import pandas as pd
-import matplotlib.pyplot as plt
-import contextily as ctx
 from bokeh.models import GeoJSONDataSource
 from bokeh.plotting import figure, show, output_file
 from bokeh.tile_providers import get_provider, Vendors
 
 
-gdal_data =  Path(r'C:\Users\nickf\.conda\envs\s57\Library\share\gdal')
-proj_lib =Path(r'C:\Users\nickf\.conda\envs\s57\Library\share\proj')
+class Enc:
 
-os.environ["GDAL_DATA"] = str(gdal_data)
-os.environ["PROJ_LIB"] = str(proj_lib)
+    def __init__(self, path):
+        self.path = path
+        self.name = path.name
 
-enc_dir = Path(r'C:\Users\nickf\Downloads\All_ENCs\ENC_ROOT')
+    def list_layers(self):
+        return fiona.listlayers(str(enc))
 
-gdfs = []
+    def __str__(self):
+        return str(self.path)
 
-s57_geojson_path = Path(r'C:\Users\nickf\Downloads\All_ENCs\s57_DEPCNT.geojson')
+    def get_objects(self, acronym, geom_type):
+        objects = gpd.read_file(str(enc), layer=acronym)
+        objects = objects[objects.geom_type == geom_type]
+        #objects = objects.loc[objects['CATZOC'] == 6]
+        objects = objects.to_crs({'init': 'epsg:3857'})
+        return objects
 
-for enc in enc_dir.rglob('US5*.000'):
 
-    #enc = Enc()
+class Shorex:
 
-    lyrs = fiona.listlayers(str(enc))
-    print(enc)
+    def __init__(self, enc_dir):
+        self.enc_dir = enc_dir
 
-    for lyr in lyrs:
-        if lyr == 'M_QUAL':
-            s57 = gpd.read_file(str(enc), layer=lyr)
-            s57 = s57.to_crs({'init': 'epsg:3857'})
-            s57 = s57.loc[s57['CATZOC'] == 6]
-            gdfs.append(s57)
+    @staticmethod
+    def set_env_vars(env_name):
+        user_dir = os.path.expanduser('~')
+        conda_dir = Path(user_dir).joinpath('AppData', 'Local', 
+                                            'Continuum', 'anaconda3')
+        env_dir = conda_dir / 'envs' / env_name
+        share_dir = env_dir / 'Library' / 'share'
+        script_path = conda_dir / 'Scripts'
+        gdal_data_path = share_dir / 'gdal'
+        proj_lib_path = share_dir
 
-rdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True), crs=gdfs[0].crs)
-rdf.to_file(str(s57_geojson_path), driver='GeoJSON')
+        if script_path.name not in os.environ["PATH"]:
+            os.environ["PATH"] += os.pathsep + str(script_path)
+        os.environ["GDAL_DATA"] = str(gdal_data_path)
+        os.environ["PROJ_LIB"] = str(proj_lib_path)
 
-output_file("tile.html")
-tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+    @staticmethod
+    def export_to_geojson(gdfs, s57_geojson_path):
+        rdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True), 
+                               crs=gdfs[0].crs)
+        rdf.to_file(str(s57_geojson_path), driver='GeoJSON')
+        print(rdf)
 
-with open(str(s57_geojson_path)) as f:
-    geojson_data = f.read()
+    @staticmethod
+    def plot_objects(s57_geojson_path):
+        output_file("tile.html")
+        tile_provider = get_provider(Vendors.CARTODBPOSITRON)
 
-s57_source = GeoJSONDataSource(geojson=geojson_data)
+        with open(str(s57_geojson_path)) as f:
+            geojson_data = f.read()
 
-p = figure(
-    title="A test map", 
-    x_range=(-2000000, 6000000), 
-    y_range=(-1000000, 7000000), 
-    x_axis_type="mercator", 
-    y_axis_type="mercator",
-    plot_width=1200,
-    plot_height=600)
-p.add_tile(tile_provider)
-#p.multi_line('xs', 'ys', source=s57_source, color='gray', line_width=2)
-#p.circle('x', 'y', source=s57_source, color='gray')
-p.patches('xs', 'ys', source=s57_source, alpha=0.4)
-##p.multi_polygons('xs', 'ys', source=s57_source, alpha=0.4)
+        source = GeoJSONDataSource(geojson=geojson_data)
 
-outfp = r"C:\Users\nickf\Downloads\All_ENCs\test_map.html"
-show(p)
+        p = figure(
+            title="A test map", 
+            x_axis_type="mercator", 
+            y_axis_type="mercator",
+            plot_width=1500,
+            plot_height=900)
+
+        p.add_tile(tile_provider)
+        #p.multi_line('xs', 'ys', source=source, color='gray', line_width=2)
+        #p.circle('x', 'y', source=source, color='gray')
+        p.patches('xs', 'ys', source=source, alpha=0.4)
+        ##p.multi_polygons('xs', 'ys', source=source, alpha=0.4)
+
+        outfp = r"C:\Users\nickf\Downloads\All_ENCs\test_map.html"
+        show(p)
+
+
+if __name__ == '__main__':
+    enc_dir = Path(r'Z:\ENCs')
+    shorex = Shorex(enc_dir)
+    shorex.set_env_vars('enc_shorex')
+    gdfs = []
+
+    for enc in shorex.enc_dir.rglob('US5*.000'):
+        enc = Enc(enc)
+        print(enc)
+        enc_objs = enc.get_objects('LNDARE', 'Polygon')
+        gdfs.append(enc_objs)
+
+    geojson_path = enc_dir / 'ENC_{}_{}.geojson'.format('LNDARE', 'Polygon')
+    shorex.export_to_geojson(gdfs, geojson_path)
+    shorex.plot_objects(geojson_path)
+
